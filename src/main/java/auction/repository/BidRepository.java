79993a23cb65e46baef28ba47b14338f18dc7cb6
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-09 15:04:40 -0300

    created bidRepository

--- src/main/java/auction/repository/BidRepository.java
+++ src/main/java/auction/repository/BidRepository.java
@@ -1,4 +1,34 @@
 package auction.repository;
 
+import auction.domain.Bid;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BidRepository {
+
+    private List<Bid> bidds = new ArrayList<>();
+
+    public BidRepository(){
+        bidds.add(new Bid ("111","Joao","0003",1000));
+    }
+
+    public Bid get(String bidId){
+        for(Bid bid: bidds)
+        {
+            if(bid.getId()==bidId)
+                return  bid;
+        }
+
+        return null;
+    }
+
+    public Bid addBid(Bid bid){
+        bidds.add(bid);
+        return bid;
+    }
+
+    public List<Bid> getAll() {
+        return bidds;
+    }
 }
