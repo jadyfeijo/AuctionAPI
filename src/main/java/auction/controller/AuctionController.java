3e6471431d776bbede19966b65ad223a953472f0
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-09 13:53:04 -0300

    created endpoint get/status

--- src/main/java/auction/controller/AuctionController.java
+++ src/main/java/auction/controller/AuctionController.java
@@ -1,5 +1,24 @@
 package auction.controller;
 
-public class AuctionController {
+import auction.domain.Auction;
+import auction.domain.enums.Status;
+import auction.service.AuctionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/Auctions")
+class AuctionController {
+
+    private final AuctionService service = new AuctionService();
+
+    @GetMapping ("/get/{status}")
+    public List<Auction> getAuctions(@PathVariable Status status){
+        return service.getByStatus(status);
+    }
 
 }
