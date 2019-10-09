96d2ecb3668ab2481b3181f1b3eaa180e4c544d3
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-09 16:30:14 -0300

    created queue endpoint

--- src/main/java/auction/controller/AuctionController.java
+++ src/main/java/auction/controller/AuctionController.java
@@ -4,6 +4,7 @@ import auction.domain.Auction;
 import auction.domain.Bid;
 import auction.domain.enums.Status;
 import auction.service.AuctionService;
+import auction.service.BidService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,26 +13,31 @@ import java.util.List;
 @RequestMapping("api/Auctions")
 class AuctionController {
 
-    private final AuctionService service = new AuctionService();
+    private final AuctionService auctionService = new AuctionService();
+    private final BidService bidService = new BidService();
 
     @GetMapping ("/get/{status}")
     public List<Auction> getAuctions(@PathVariable Status status){
-        return service.getByStatus(status);
+        return auctionService.getByStatus(status);
     }
 
     @PutMapping("/create")
     public Auction createAuction(@RequestBody AuctionRequest auction){
-        return service.createAuction(auction.item);
+        return auctionService.createAuction(auction.item);
     }
 
-    @PostMapping("/newBid/{auctionId}")
+    @PostMapping("/{auctionId}/newBid")
     public Auction newBid(@PathVariable String auctionId,@RequestBody BidRequest bid){
 
         Bid newBid = new Bid(bid.bidderId,bid.name,auctionId,bid.bid);
 
-        return service.addBid(newBid);
+        return auctionService.addBid(newBid);
     }
 
+    @GetMapping("/{auctionId}/queue")
+    public List<Bid> getQueue(@PathVariable String auctionId){
+        return bidService.getByAuction(auctionId);
+    }
 }
 
 class AuctionRequest{
