e9fadc9997bdca32fbc36012718688ce2cddbf27
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-09 15:05:19 -0300

    created bidService class

--- src/main/java/auction/service/BidService.java
+++ src/main/java/auction/service/BidService.java
@@ -1,4 +1,19 @@
 package auction.service;
 
+import auction.domain.Bid;
+import auction.repository.BidRepository;
+
+import java.util.List;
+
 public class BidService {
+
+    private final BidRepository repo = new BidRepository();
+
+    public Bid get(String id){
+        return repo.get(id);
+    }
+
+    public List<Bid> getAll(){
+        return repo.getAll();
+    }
 }
