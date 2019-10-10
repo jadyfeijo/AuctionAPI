fa6ff6024c65204026084351afb76f7bae4a7b60
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-10 16:25:53 -0300

    created bid controller class

--- src/main/java/auction/controller/BidController.java
+++ src/main/java/auction/controller/BidController.java
@@ -1,4 +1,33 @@
 package auction.controller;
 
-public class BidController {
+import auction.domain.Bid;
+import auction.service.BidService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/Bidds")
+public class BidController{
+
+
+    private final static BidService service = new BidService();
+
+        @GetMapping("/{auctionId}/queue")
+        public List<Bid> getQueue(@PathVariable String auctionId){
+            return service.getByAuction(auctionId);
+        }
+
+        @PutMapping("{auctionId}/confirm/{bidderId}")
+        public Bid confirm(@PathVariable String auctionId, @PathVariable String bidderId){
+            return service.confirm(auctionId,bidderId);
+        }
+
+        @PutMapping("{auctionId}/recuse/{bidderId}")
+        public Bid recuse (@PathVariable String auctionId, @PathVariable String bidderId){
+            return service.recuse(auctionId,bidderId);
+        }
+
 }
+
