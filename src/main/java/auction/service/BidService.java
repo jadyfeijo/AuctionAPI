905ab4590e114fd2425d85b69cd60966df321029
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-10 16:27:05 -0300

    created confirm, recuse methods

--- src/main/java/auction/service/BidService.java
+++ src/main/java/auction/service/BidService.java
@@ -1,5 +1,6 @@
 package auction.service;
 
+import auction.domain.Auction;
 import auction.domain.Bid;
 import auction.repository.BidRepository;
 
@@ -8,6 +9,7 @@ import java.util.List;
 public class BidService {
 
     private final static  BidRepository repo = new BidRepository();
+    private final static AuctionService auctionService = new AuctionService();
 
     public Bid get(String id){
         return repo.get(id);
@@ -18,10 +20,66 @@ public class BidService {
     }
 
     public Bid addBid (Bid bid){
-        return repo.addBid(bid);
+        return repo.save(bid);
     }
 
     public List<Bid> getByAuction(String auctionId) {
         return repo.getByAuction(auctionId);
+    }
+
+    public Bid confirm(String auctionId,String bidderId) {
+
+        Bid highestBid = repo.getHighestOffer(auctionId);
+
+        if(highestBid.getBidderId().equals(bidderId)){
+            highestBid.setBuyer(true);
+            return repo.save(highestBid);
+        }
+        else{
+            throw new RuntimeException("Your bid is not the highest");
+        }
+
+
+
+
+    }
+
+    public Bid recuse(String auctionId, String bidderId) {
+
+        Bid recusedBid = getLastBid(auctionId,bidderId);
+
+        recusedBid.setPossibleBuyer(false);
+        repo.save(recusedBid);
+
+        Auction auction = auctionService.get(auctionId);
+
+        if(auction.getHighestOffer()==recusedBid.getBid()){
+            auction.setHighestOffer(repo.getHighestOffer(auction.getId()).getBid());
+            auctionService.save(auction);
+        }
+
+        return recusedBid;
+    }
+
+    public Bid getLastBid(String auctionId,String bidderId){
+        Bid bid = repo.getLastBid(auctionId,bidderId);
+
+        if(bid.getId()!=null)
+            return bid;
+
+        else
+            throw new RuntimeException("This Auction/Bidder has no Bid yet");
+
+    }
+
+    public  Bid getHighestOffer(String auctionId){
+        Bid bid = repo.getHighestOffer(auctionId);
+
+        if(bid.getId()!=null)
+            return bid;
+
+        else
+            throw new RuntimeException("This Auction has no Bid yet");
+
     }
 }
