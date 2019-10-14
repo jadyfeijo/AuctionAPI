e8f2594b77d397f4f4b164456376f733a788e5f7
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2019-10-14 18:07:22 -0300

    created Auction Service getByStatus methods tests

--- src/test/java/auction/service/AuctionServiceTest.java
+++ src/test/java/auction/service/AuctionServiceTest.java
@@ -4,15 +4,21 @@ import auction.domain.Auction;
 import auction.domain.Bid;
 import auction.domain.enums.Status;
 import auction.repository.AuctionRepository;
+import auction.repository.BidRepository;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import javax.validation.constraints.AssertTrue;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 
@@ -22,8 +28,8 @@ public class AuctionServiceTest {
     @Test(expected = RuntimeException.class)
     public void addBid_willPass_whenBidValueIsLessThanAuctioHighestOffer() {
         AuctionRepository auctionRepository = mock(AuctionRepository.class);
-        BidService bidService = mock(BidService.class);
-        AuctionService service = new AuctionService(auctionRepository, bidService);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService service = new AuctionService(auctionRepository, bidRepository);
 
         Bid newBid = new Bid();
         newBid.setAuctionId("0001");
@@ -41,8 +47,8 @@ public class AuctionServiceTest {
     @Test
     public void addBid_willPass_whenAuctionHighestOfferrReceiveBidValue() {
         AuctionRepository auctionRepository = mock(AuctionRepository.class);
-        BidService bidService = mock(BidService.class);
-        AuctionService service = new AuctionService(auctionRepository, bidService);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService service = new AuctionService(auctionRepository, bidRepository);
 
         Bid newBid = new Bid();
         newBid.setAuctionId("0001");
@@ -63,8 +69,8 @@ public class AuctionServiceTest {
     @Test
     public void createAuction_shouldPass_whenReturnedAuctionHasOpenStatus() {
         AuctionRepository auctionRepository = mock(AuctionRepository.class);
-        BidService bidService = mock(BidService.class);
-        AuctionService service = new AuctionService(auctionRepository, bidService);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService service = new AuctionService(auctionRepository, bidRepository);
 
         service.createAuction("item");
 
@@ -75,8 +81,8 @@ public class AuctionServiceTest {
     @Test
     public void generateAuction_shouldPass_whenReturnedAuctionHasOpenStatusAndItemEqualsItemParameter() {
         AuctionRepository auctionRepository = mock(AuctionRepository.class);
-        BidService bidService = mock(BidService.class);
-        AuctionService service = new AuctionService(auctionRepository, bidService);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService service = new AuctionService(auctionRepository, bidRepository);
 
         Auction auction = service.generateAuction("ITEM NAME");
 
@@ -87,11 +93,48 @@ public class AuctionServiceTest {
     }
 
     @Test
-    public void getByStatus_shouldPass_ifStatusParameterIsOpenThanAllAuctionReturnedHasOpenStatus() {
+    public void getByStatus_shouldPass_ifStatusParameterIsOpenAndTheAuctionInicialDateplus15minIsAfterNowDateThenRepoShouldSaveAnAuction() throws ParseException {
         AuctionRepository auctionRepository = mock(AuctionRepository.class);
-        BidService bidService = mock(BidService.class);
-        AuctionService auctionService = new AuctionService(auctionRepository, bidService);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService auctionService = new AuctionService(auctionRepository, bidRepository);
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
+        Status status = Status.OPEN;
+
+        List<Auction> auctions=new ArrayList<>();
+
+        auctions.add(new Auction("000","item1",200,Status.OPEN,sdf.parse("14/10/2019 17:30")));
+
+        when(auctionRepository.getByStatus(status)).thenReturn(auctions);
+
+        auctionService.getByStatus(status);
+
+        then(auctionRepository).should().getByStatus(status);
+
+        then(auctionRepository).should(times(1)).save(any(Auction.class));
     }
+
+    @Test
+    public void getByStatus_shouldPass_ifStatusParameterIsOpenAndTheAuctionInicialDateIsAfterNowThenRepoShoulNotSave(){
+        AuctionRepository auctionRepository = mock(AuctionRepository.class);
+        BidRepository bidRepository = mock(BidRepository.class);
+        AuctionService auctionService = new AuctionService(auctionRepository, bidRepository);
+
+        Status status = Status.OPEN;
+
+        List<Auction> auctions=new ArrayList<>();
+
+        auctions.add(new Auction("000","item1",200,Status.OPEN,new Date()));
+
+        when(auctionRepository.getByStatus(status)).thenReturn(auctions);
+
+        auctionService.getByStatus(status);
+
+        then(auctionRepository).should().getByStatus(status);
+
+        then(auctionRepository).should(times(0)).save(any(Auction.class));
+    }
+
+
 }
